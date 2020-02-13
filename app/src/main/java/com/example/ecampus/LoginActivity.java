8027c9c59252d1400a1ed6d42e545f12c9099529
akshay1552 <akshaykudtarkar9892@gmail.com>
2020-02-13 21:28:51 +0530

    eCAMPUS

--- app/src/main/java/com/example/ecampus/LoginActivity.java
+++ app/src/main/java/com/example/ecampus/LoginActivity.java
@@ -2,14 +2,18 @@ package com.example.ecampus;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.view.animation.OvershootInterpolator;
 import android.widget.LinearLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
     //layout declaration
     private LinearLayout layout1;
     private LinearLayout layout2;
@@ -23,11 +27,20 @@ public class LoginActivity extends AppCompatActivity {
     Animation btnanim3;
     Animation btnanim4;
 
+    //floating action button
+    FloatingActionButton floatmain, float1, float2, float3;
+    float translationY = 100;
+    Boolean isMenuOpen = false;
+    private static final String TAG = "LoginActivity";
+    OvershootInterpolator interpolator = new OvershootInterpolator();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        initFloatMenu();
 
         //declare the animation
         btnanim1 = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.btt1);
@@ -55,7 +68,75 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+    }
 
+    private void initFloatMenu() {
+
+        //declare floating buttons
+        floatmain = findViewById(R.id.floatingb);
+        float1 = findViewById(R.id.floatingb1);
+        float2 = findViewById(R.id.floatingb2);
+        float3 = findViewById(R.id.floatingb3);
+
+        float1.setAlpha(0f);
+        float2.setAlpha(0f);
+        float3.setAlpha(0f);
+
+        float1.setTranslationY(translationY);
+        float2.setTranslationY(translationY);
+        float3.setTranslationY(translationY);
+
+
+        //setup floating buttons
+        floatmain.setOnClickListener(this);
+        float1.setOnClickListener(this);
+        float2.setOnClickListener(this);
+        float3.setOnClickListener(this);
+
+    }
+
+    private void openMenu(){
+
+        isMenuOpen = !isMenuOpen;
+        floatmain.animate().setInterpolator(interpolator).rotation(45f).setDuration(300).start();
+        float1.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+        float2.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+        float3.animate().translationY(0f).alpha(1f).setInterpolator(interpolator).setDuration(300).start();
+
+    }
+
+    private void closeMenu(){
+
+        isMenuOpen = !isMenuOpen;
+        floatmain.animate().setInterpolator(interpolator).rotation(0f).setDuration(300).start();
+        float1.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+        float2.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+        float3.animate().translationY(translationY).alpha(0f).setInterpolator(interpolator).setDuration(300).start();
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+            case R.id.floatingb:
+                Log.i(TAG, "onClick: Float main");
+                if (isMenuOpen){
+                    closeMenu();
+                }else {
+                    openMenu();
+                }
+                break;
+            case R.id.floatingb1:
+                Log.i(TAG, "onClick:College");
+                break;
+            case R.id.floatingb2:
+                Log.i(TAG, "onClick: Courses");
+                break;
+            case R.id.floatingb3:
+                Log.i(TAG, "onClick: About");
+                break;
+        }
 
     }
 }
