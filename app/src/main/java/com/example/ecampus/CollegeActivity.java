4925e031a6f424917534c9a766739244a10d4104
akshay1552 <akshaykudtarkar9892@gmail.com>
2020-02-19 21:12:23 +0530

    eCAMPUS

---
+++ app/src/main/java/com/example/ecampus/CollegeActivity.java
@@ -0,0 +1,14 @@
+package com.example.ecampus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CollegeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college);
+    }
+}
