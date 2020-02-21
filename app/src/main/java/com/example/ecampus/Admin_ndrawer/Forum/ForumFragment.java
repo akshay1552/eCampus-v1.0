3be26c1d922b1acf839d871f66874999d0d92703
akshay1552 <akshaykudtarkar9892@gmail.com>
2020-02-21 15:52:53 +0530

    eCAMPUS

--- app/src/main/java/com/example/ecampus/Admin_ndrawer/Forum/ForumFragment.java
+++ app/src/main/java/com/example/ecampus/Admin_ndrawer/Forum/ForumFragment.java
@@ -4,32 +4,18 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.example.ecampus.R;
 
 public class ForumFragment extends Fragment {
 
-    private ForumViewModel forumViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        forumViewModel =
-                ViewModelProviders.of(this).get(ForumViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_forum, container, false);
-        final TextView textView = root.findViewById(R.id.text_forum);
-        forumViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_forum,container,false);
     }
 }
